61a91b069c78ddbbed03e9d1d42c8d7fc4ee60c2
Hervé Boutemy <hboutemy@apache.org>
2014-08-19 17:27:45 +0200

    fixed typo in information message

--- indexer-cli/src/main/java/org/apache/maven/index/cli/NexusIndexerCli.java
+++ indexer-cli/src/main/java/org/apache/maven/index/cli/NexusIndexerCli.java
@@ -207,7 +207,7 @@ public class NexusIndexerCli
         .withDescription( "Create checksums for all files (sha1, md5)." ).create( CREATE_FILE_CHECKSUMS ) );
 
         options.addOption( OptionBuilder.withLongOpt( "type" ).hasArg() //
-        .withDescription( "Indexer type (default, min, full or coma separated list of custom types)." ).create( TYPE ) );
+        .withDescription( "Indexer type (default, min, full or comma separated list of custom types)." ).create( TYPE ) );
 
         options.addOption( OptionBuilder.withLongOpt( "legacy" ) //
         .withDescription( "Build legacy .zip index file" ).create( LEGACY ) );
