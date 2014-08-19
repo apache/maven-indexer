7e7202d774596eea07a5c905e92b2e9b80a17543
Hervé Boutemy <hboutemy@apache.org>
2014-08-19 21:40:20 +0200

    [MINDEXER-90] created CLI options reference

--- indexer-cli/src/test/java/org/apache/maven/index/cli/NexusIndexerCliTest.java
+++ indexer-cli/src/test/java/org/apache/maven/index/cli/NexusIndexerCliTest.java
@@ -19,9 +19,17 @@ package org.apache.maven.index.cli;
  * under the License.
  */
 
+import java.io.File;
+import java.io.IOException;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
+import org.apache.commons.cli.Option;
 import org.apache.maven.index.cli.NexusIndexerCli;
+import org.codehaus.plexus.util.FileUtils;
 
 public class NexusIndexerCliTest
     extends AbstractNexusIndexerCliTest
@@ -47,4 +55,62 @@ public class NexusIndexerCliTest
         return cli.execute( args );
     }
 
+    private final static String LS = System.getProperty( "line.separator" );
+
+    private static class OptionComparator
+        implements Comparator<Option>
+    {
+        public int compare( Option opt1, Option opt2 )
+        {
+            return opt1.getOpt().compareToIgnoreCase( opt2.getOpt() );
+        }
+    }
+
+    public String getOptionsAsHtml()
+    {
+        @SuppressWarnings( "unchecked" )
+        List<Option> optList = new ArrayList<Option>( new NexusIndexerCli().buildDefaultCliOptions().getOptions() );
+        Collections.sort( optList, new OptionComparator() );
+
+        StringBuilder sb = new StringBuilder();
+        boolean a = true;
+        sb.append( "<table border='1' class='zebra-striped'><tr class='a'><th><b>Options</b></th><th><b>Description</b></th></tr>" );
+        for ( Option option : optList )
+        {
+            a = !a;
+            sb.append( "<tr class='" ).append( a ? 'a' : 'b' ).append( "'><td><code>-<a name='" );
+            sb.append( option.getOpt() );
+            sb.append( "'>" );
+            sb.append( option.getOpt() );
+            sb.append( "</a>,--<a name='" );
+            sb.append( option.getLongOpt() );
+            sb.append( "'>" );
+            sb.append( option.getLongOpt() );
+            sb.append( "</a>" );
+            if ( option.hasArg() )
+            {
+                if ( option.hasArgName() )
+                {
+                    sb.append( " &lt;" ).append( option.getArgName() ).append( "&gt;" );
+                }
+                else
+                {
+                    sb.append( ' ' );
+                }
+            }
+            sb.append( "</code></td><td>" );
+            sb.append( option.getDescription() );
+            sb.append( "</td></tr>" );
+            sb.append( LS );
+        }
+        sb.append( "</table>" );
+        return sb.toString();
+    }
+
+    public void testOptionsAsHtml()
+        throws IOException
+    {
+        File options = getTestFile( "target/test-classes/options.html" );
+        FileUtils.fileWrite( options, "UTF-8", getOptionsAsHtml() );
+    }
 }
