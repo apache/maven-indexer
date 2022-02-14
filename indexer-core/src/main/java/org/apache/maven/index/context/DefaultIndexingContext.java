apache/maven-indexer

commit 019edb7ff87a505372ad4f469c1b2a6c9d4ae292

[MINDEXER-132] use Java 7 Files.createTempDirectory() API (#164)
Co-authored-by: Tamas Cservenak <tamas@cservenak.net>

indexer-core/src/main/java/org/apache/maven/index/context/DefaultIndexingContext.java
@@ -216,12 +216,9 @@ public class DefaultIndexingContext
     {
         if ( dir == null )
         {
-            // best effort, to have a directory thru the life of a ctx
-            File tmpFile = File.createTempFile( "mindexer-ctx" + id, "tmp" );
-            tmpFile.deleteOnExit();
-            tmpFile.delete();
-            tmpFile.mkdirs();
-            this.indexDirectoryFile = tmpFile;
+            // best effort, to have a directory through the life of a ctx
+            this.indexDirectoryFile = Files.createTempDirectory( "mindexer-ctx" + id ).toFile();
+            this.indexDirectoryFile.deleteOnExit();
         }
         else
         {

indexer-core/src/main/java/org/apache/maven/index/DefaultNexusIndexer.java
@@ -24,6 +24,7 @@ import javax.inject.Named;
 import javax.inject.Singleton;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -269,14 +270,7 @@ public class DefaultNexusIndexer
         }
 
         // always use temporary context when reindexing
-        final File tmpFile = File.createTempFile( context.getId() + "-tmp", "" );
-        tmpFile.deleteOnExit();
-        final File tmpDir = new File( tmpFile.getParentFile(), tmpFile.getName() + ".dir" );
-        if ( !tmpDir.mkdirs() )
-        {
-            throw new IOException( "Cannot create temporary directory: " + tmpDir );
-        }
-
+        final File tmpDir = Files.createTempDirectory( context.getId() + "-tmp" ).toFile();
         IndexingContext tmpContext = null;
         try
         {
@@ -310,11 +304,6 @@ public class DefaultNexusIndexer
             if ( tmpContext != null )
             {
                 tmpContext.close( true );
-            }
-
-            if ( tmpFile.exists() )
-            {
-                tmpFile.delete();
             }
 
             FileUtils.deleteDirectory( tmpDir );

indexer-core/src/main/java/org/apache/maven/index/context/MergedIndexingContext.java
@@ -21,6 +21,7 @@ package org.apache.maven.index.context;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
@@ -308,12 +309,9 @@ public class MergedIndexingContext
     {
         if ( dir == null )
         {
-            // best effort, to have a directory thru the life of a ctx
-            File tmpFile = File.createTempFile( "mindexer-ctx" + id, "tmp" );
-            tmpFile.deleteOnExit();
-            tmpFile.delete();
-            tmpFile.mkdirs();
-            this.directoryFile = tmpFile;
+            // best effort, to have a directory through the life of a ctx
+            this.directoryFile = Files.createTempDirectory( "mindexer-ctx" + id ).toFile();
+            this.directoryFile.deleteOnExit();
         }
         else
         {

indexer-core/src/main/java/org/apache/maven/index/updater/DefaultIndexUpdater.java
@@ -35,6 +35,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.nio.file.Files;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -186,14 +187,17 @@ public class DefaultIndexUpdater
                                      final boolean merge, final String remoteIndexFile )
         throws IOException
     {
+        File indexDir;
         if ( updateRequest.getIndexTempDir() != null )
         {
             updateRequest.getIndexTempDir().mkdirs();
+            indexDir = Files.createTempDirectory( updateRequest.getIndexTempDir().toPath(),
+                remoteIndexFile + ".dir" ).toFile();
         }
-        File indexDir = File.createTempFile( remoteIndexFile, ".dir" , updateRequest.getIndexTempDir() );
-        indexDir.delete();
-        indexDir.mkdirs();
-
+        else
+        {
+            indexDir = Files.createTempDirectory( remoteIndexFile + ".dir" ).toFile();
+        }
         try ( BufferedInputStream is = new BufferedInputStream( fetcher.retrieve( remoteIndexFile ) ); //
                         Directory directory = updateRequest.getFSDirectoryFactory().open( indexDir ) )
         {

indexer-core/src/main/java/org/apache/maven/index/updater/WagonHelper.java
@@ -37,6 +37,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
 
 /**
  * This is a helper for obtaining Wagon based ResourceFetchers. Some Indexer integrations does have access to Wagon
@@ -191,7 +192,7 @@ public class WagonHelper
         public InputStream retrieve( String name )
             throws IOException, FileNotFoundException
         {
-            final File target = File.createTempFile( name, "" );
+            final File target = Files.createTempFile( name, "tmp" ).toFile();
             target.deleteOnExit();
             retrieve( name, target );
             return new FileInputStream( target )

indexer-core/src/test/java/org/apache/maven/index/updater/FullBootProofOfConcept.java
@@ -21,6 +21,7 @@ package org.apache.maven.index.updater;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
@@ -48,7 +49,7 @@ public class FullBootProofOfConcept
     {
         for ( int i = 0; i < 1; i++ )
         {
-            File basedir = File.createTempFile( "nexus-indexer.", ".dir" );
+            File basedir = Files.createTempDirectory("nexus-indexer..dir" ).toFile();
             basedir.deleteOnExit();
 
             try

indexer-reader/src/test/java/org/apache/maven/index/reader/TestSupport.java
@@ -27,6 +27,8 @@ import org.junit.rules.TestName;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -54,9 +56,7 @@ public class TestSupport
     public void setup()
         throws IOException
     {
-        this.tempDir = new File( "target/tmp-" + getClass().getSimpleName() );
-        this.tempDir.delete();
-        this.tempDir.mkdirs();
+        this.tempDir = Files.createTempDirectory( getClass().getSimpleName() + ".temp" ).toFile();
         this.directoryResourceHandlers = new ArrayList<>();
     }
 
@@ -91,10 +91,7 @@ public class TestSupport
     protected File createTempDirectory()
         throws IOException
     {
-        File result = File.createTempFile( testName.getMethodName() + "-dir", "", tempDir );
-        result.delete();
-        result.mkdirs();
-        return result;
+        return Files.createTempDirectory( tempDir.toPath(), testName.getMethodName() + "-dir" ).toFile();
     }
 
     /**

commit 28fd31aeeb0aa181643b2f87073d8009c3c3b8bf

[MINDEXER-140] Fix double invocation of DefaultIndexingContext.setIndexDirectoryFile (#178)
DefaultIndexingContext invokes method setIndexDirectoryFile two times (if indexDirectory set): once with null and once with given indexDirectory.

In first case, ID is used to create temp directory, just to have 2nd invocation use passed in directory (and leave temp directory created in 1st invocation alone).

This bug, in relation to commit 019edb7ff87a505372ad4f469c1b2a6c9d4ae292 broke Spring examples, as it seems Spring example contained a typo since beginning: it uses "/" (slash) in context ID.

Files.createTempDirectory fails if prefix contains path separator.

---

https://issues.apache.org/jira/browse/MINDEXER-140

indexer-core/src/main/java/org/apache/maven/index/context/DefaultIndexingContext.java
@@ -125,7 +125,8 @@ public class DefaultIndexingContext
                                     String repositoryUrl, String indexUpdateUrl,
                                     List<? extends IndexCreator> indexCreators, Directory indexDirectory,
                                     TrackingLockFactory lockFactory,
-                                    boolean reclaimIndex )
+                                    boolean reclaimIndex,
+                                    File indexDirectoryFile )
         throws ExistingLuceneIndexMismatchException, IOException
     {
 
@@ -164,7 +165,7 @@ public class DefaultIndexingContext
 
         prepareIndex( reclaimIndex );
 
-        setIndexDirectoryFile( null );
+        setIndexDirectoryFile( indexDirectoryFile );
     }
 
     private DefaultIndexingContext( String id, String repositoryId, File repository, File indexDirectoryFile,
@@ -173,9 +174,8 @@ public class DefaultIndexingContext
         throws IOException, ExistingLuceneIndexMismatchException
     {
         this( id, repositoryId, repository, repositoryUrl, indexUpdateUrl, indexCreators,
-            FSDirectory.open( indexDirectoryFile.toPath(), lockFactory ), lockFactory, reclaimIndex );
-
-        setIndexDirectoryFile( indexDirectoryFile );
+            FSDirectory.open( indexDirectoryFile.toPath(), lockFactory ), lockFactory, reclaimIndex,
+                indexDirectoryFile );
     }
 
     public DefaultIndexingContext( String id, String repositoryId, File repository, File indexDirectoryFile,
@@ -194,12 +194,9 @@ public class DefaultIndexingContext
         throws IOException, ExistingLuceneIndexMismatchException
     {
         this( id, repositoryId, repository, repositoryUrl, indexUpdateUrl, indexCreators, indexDirectory, null,
-              reclaimIndex ); // Lock factory already installed - pass null
-
-        if ( indexDirectory instanceof FSDirectory )
-        {
-            setIndexDirectoryFile( ( (FSDirectory) indexDirectory ).getDirectory().toFile() );
-        }
+              reclaimIndex, indexDirectory instanceof FSDirectory
+                        ? ( (FSDirectory) indexDirectory ).getDirectory().toFile() : null
+        ); // Lock factory already installed - pass null
     }
 
     public Directory getIndexDirectory()

indexer-examples/indexer-examples-spring/src/main/java/org/apache/maven/index/examples/indexing/RepositoryIndexerFactory.java
@@ -65,7 +65,7 @@ public class RepositoryIndexerFactory
     private IndexingContext createIndexingContext( String repositoryId, File repositoryBasedir, File indexDir )
         throws IOException
     {
-        return getIndexer().createIndexingContext( repositoryId + "/ctx", repositoryId, repositoryBasedir, indexDir,
+        return getIndexer().createIndexingContext( repositoryId + "-ctx", repositoryId, repositoryBasedir, indexDir,
                                                    null, null, true,
                                                    // if context should be searched in non-targeted mode.
                                                    true, // if indexDirectory is known to contain (or should contain)